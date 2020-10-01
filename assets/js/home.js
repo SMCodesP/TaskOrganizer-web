acc39bb034f8f5fcb25468f62990b640fcb4ba34
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-10-01 17:03:41 -0300

    Finish implementation system of login

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,3 +3,37 @@ const addNewTask = document.getElementById('addNewTask')
 addNewTask.onclick = () => {
   
 }
+
+window.onload = async () => {
+  const token = localStorage.getItem('token')
+  const container_tasks = document.getElementById('container-tasks')
+
+  try {
+    const response = await axios.get('https://tasks-organizer.herokuapp.com/tasks', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    console.log(response)
+
+    response.data.forEach((task) => {
+      container_tasks.innerHTML += `
+        <div class="card">
+          <div class="card-header">
+            Banco de dados
+          </div>
+          <div class="card-body">
+            <p class="text-muted">Prazo 29/09/2020</p>
+            <h2 class="card-title">Modelo conceitual, lógico e físico</h2>
+            <p class="card-text">Utilize um dos softwares explicado em aula para realizar um modelo de entidade relacionamento, modelo lógico e o modelo físico do estudo de caso abaixo. Após o termino você poderá postar um print dos diagramas ou dos arquivos gerados pelo app. Faça o modelo conceitual, modelo lógico e modelo físico do estudo de caso abaixo:</p>
+            <a href="#" class="btn btn-primary">Ver mais</a>
+          </div>
+        </div>
+      `
+    })
+  } catch (error) {
+    console.log(error)
+    alert((!error.response || error.response.status === 500) ? 'Houve um erro desconhecido, contate aos desenvolvedores.' : error.response.data)
+  }
+}
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,29 @@
 const form = document.getElementById('login-submit')
 
-form.onsubmit = (event) => {
-  alert("logou")
+form.onsubmit = async (event) => {
+  event.preventDefault()
+  
+  const {value: username} = document.querySelector("input[data-type=username]")
+  const {value: password} = document.querySelector("input[data-type=password]")
+
+  if (!username || !password) {
+    alert('Por favor digite seu nome e sua senha.')
+    return false;
+  }
+
+  try {
+    const response = await axios.post('https://tasks-organizer.herokuapp.com/session', {
+      username,
+      password,
+    })
+
+    console.log(response.data.token)
+    localStorage.setItem('token', response.data.token)
+    window.location.replace("/dashboard");
+  } catch (error) {
+    console.log(error)
+    alert((!error.response || error.response.status === 500) ? 'Houve um erro desconhecido, contate aos desenvolvedores.' : error.response.data)
+  }
+
+  return false;
 }
