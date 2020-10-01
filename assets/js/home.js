3070b5c36a80ee7bcf0283ff0bdbebe94ab79ec5
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-10-01 17:12:35 -0300

    Finish implementation system of loading tasks

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,12 +1,7 @@
-const addNewTask = document.getElementById('addNewTask')
-
-addNewTask.onclick = () => {
-  
-}
-
 window.onload = async () => {
   const token = localStorage.getItem('token')
   const container_tasks = document.getElementById('container-tasks')
+  const quantity_task = document.getElementById('quantity-task')
 
   try {
     const response = await axios.get('https://tasks-organizer.herokuapp.com/tasks', {
@@ -17,21 +12,28 @@ window.onload = async () => {
 
     console.log(response)
 
+    if (response.data.length < 1) {
+      container_tasks.innerHTML += `
+        <h1 class="empty-task">Você não tem nenhuma tarefa.</h1>
+      `
+    }
+
     response.data.forEach((task) => {
       container_tasks.innerHTML += `
-        <div class="card">
+        <div class="card task">
           <div class="card-header">
-            Banco de dados
+            ${task.matter_title}
           </div>
           <div class="card-body">
             <p class="text-muted">Prazo 29/09/2020</p>
-            <h2 class="card-title">Modelo conceitual, lógico e físico</h2>
-            <p class="card-text">Utilize um dos softwares explicado em aula para realizar um modelo de entidade relacionamento, modelo lógico e o modelo físico do estudo de caso abaixo. Após o termino você poderá postar um print dos diagramas ou dos arquivos gerados pelo app. Faça o modelo conceitual, modelo lógico e modelo físico do estudo de caso abaixo:</p>
+            <h2 class="card-title">${task.task_title}</h2>
+            <p class="card-text">${task.description}</p>
             <a href="#" class="btn btn-primary">Ver mais</a>
           </div>
         </div>
       `
     })
+    quantity_task.innerHTML = response.data.length
   } catch (error) {
     console.log(error)
     alert((!error.response || error.response.status === 500) ? 'Houve um erro desconhecido, contate aos desenvolvedores.' : error.response.data)
