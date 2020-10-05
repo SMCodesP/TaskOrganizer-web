a964b76fe01016705a0cec6cfaf5b50b7f087569
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-10-05 17:36:44 -0300

    Implementation of take out task and unique list

--- assets/js/home.js
+++ assets/js/home.js
@@ -7,7 +7,44 @@ function openModalTask(modal) {
   $(modal).modal('show')
 }
 
-function completeTask(task) {
+async function takeOutTask(task) {
+  try {
+    await axios.put(`https://tasks-organizer.herokuapp.com/task/${task}?status=false`, null, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+
+    tasks = tasks.map((task_origin) => {
+      const task_changed = {...task_origin, completed: !task_origin.completed}
+      return (task_origin.id === task) ? task_changed : task_origin
+    })
+  
+    renderTasks()
+  } catch (err) {
+    console.log(err.response)
+    alert(`Erro » ${err.response.data.validation.body.message}`)
+  }
+}
+
+async function completeTask(task) {
+  try {
+    await axios.put(`https://tasks-organizer.herokuapp.com/task/${task}?status=true`, null, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+
+    tasks = tasks.map((task_origin) => {
+      const task_changed = {...task_origin, completed: !task_origin.completed}
+      return (task_origin.id === task) ? task_changed : task_origin
+    })
+  
+    renderTasks()
+  } catch (err) {
+    console.log(err.response)
+    alert(`Erro » ${err.response.data.validation.body.message}`)
+  }
 }
 
 function renderTasks(completed_local) {
@@ -24,17 +61,19 @@ function renderTasks(completed_local) {
   if (!completed) {
     $('#closed').removeClass('selected_completed')
     $('#waiting').addClass('selected')
-    tasks_locate = tasks
+    tasks_locate = tasks.filter((task) => !task.completed)
   } else {
     $('#closed').addClass('selected_completed')
     $('#waiting').removeClass('selected')
-    tasks_locate = tasks_completed
+    tasks_locate = tasks.filter((task) => task.completed)
   }
   
-  quantity_task.innerHTML = tasks_locate.length
-  quantity_task_completed.innerHTML = tasks_completed.length
+  quantity_task.innerHTML = tasks.filter((task) => !task.completed).length
+  quantity_task_completed.innerHTML = tasks.filter((task) => task.completed).length
 
   if (tasks_locate.length < 1) {
+    container_tasks.innerHTML = ''
+    model_task.innerHTML = ''
     container_tasks.innerHTML += `
       <h1 class="empty-task">Você não tem nenhuma tarefa.</h1>
     `
@@ -73,7 +112,7 @@ function renderTasks(completed_local) {
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-              <button onclick="completeTask(${task.id})" type="submit" class="btn btn-primary">Concluir</button>
+              <button data-dismiss="modal" onclick="${task.completed ? `takeOutTask(${task.id})` : `completeTask(${task.id})`}" type="submit" class="btn ${task.completed ? 'btn-danger' : 'btn-primary'}">${task.completed ? 'Desfazer entrega' : 'Concluir'}</button>
             </div>
           </div>
         </div>
@@ -83,7 +122,7 @@ function renderTasks(completed_local) {
       container_tasks.innerHTML += `
         <div class="card task">
           <div class="card-header">
-            ${task.matter_title} ${(timestamp < new Date().valueOf()) ? `<span style="font-family: sans-serif;" class="badge badge-danger">Em atraso</span>` : ''}
+            ${task.matter_title} ${(timestamp < new Date().valueOf()) ? `<span style="font-family: sans-serif;" class="badge badge-danger">${task.completed ? 'Entregue em atraso' : 'Em atraso'}</span>` : ''}
           </div>
           <div class="card-body">
             <p class="text-muted">Prazo ${moment(task.due_timestamp).format('HH:mm:ss - DD/MM/YYYY')}</p>
@@ -149,8 +188,7 @@ window.onload = async () => {
       }
     })
 
-    tasks_completed = [...tasks_completed, ...responseCompleted.data]
-    tasks = [...tasks, ...response.data]
+    tasks = [...tasks, ...response.data, ...responseCompleted.data]
     renderTasks(false)
   } catch (error) {
     console.log(error)
@@ -169,8 +207,8 @@ function readURL(input) {
 		var reader = new FileReader();
 		
 		reader.onload = async (e) => {
-      const response = prompt('Você tem certeza que quer trocar seu avatar? (Sim/Não)')
-      if (response.toLowerCase() === "sim" || response.toLowerCase() === "yes") {
+      const response = confirm('Você tem certeza que quer trocar seu avatar?\nClique em `Cancel` para cancelar a troca ou clique em `Ok` para confirmar.')
+      if (response) {
         try {
           const token = localStorage.getItem('token')
           let user = JSON.parse(localStorage.getItem('user'))
