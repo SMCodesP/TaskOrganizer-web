38d45d896ea811b25116e7599ded5529650338c7
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-10-06 23:32:54 -0300

    Read top #10 points and #3

---
+++ assets/js/top.js
@@ -0,0 +1,26 @@
+window.onload = async () => {
+
+  try {
+    const {data: positions} = await axios.get('https://tasks-organizer.herokuapp.com/podium')
+
+    positions.forEach((position, index) => {
+      $('#positions')[0].innerHTML += `
+        <div class="containerTable">
+          <tdCustom rowspan="1">${index+1}</tdCustom>
+          <tdCustom rowspan="2"><img class="avatar-table" src="${position.avatar_url}" /> ${position.username}</tdCustom>
+          <tdCustom rowspan="1">${position.points}</tdCustom>
+        </div>
+      `
+    })
+
+    positions.filter((_, index) => (index < 3)).forEach((position, index) => {
+      $(`#username-top-${index+1}`)[0].innerHTML = position.username
+      $(`#points-top-${index+1}`)[0].innerHTML = position.points
+      $(`#avatar-top-${index+1}`)[0].src = position.avatar_url
+    })
+
+  } catch (err) {
+    alert('Houve um erro ao carregar o podium.')
+  }
+
+}
